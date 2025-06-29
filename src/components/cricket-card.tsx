f7f9aa7116a598c4216865da04af7923e7b019ce
rpmakki03 <rpmakki03@gmail.com>
2025-06-29 14:35:30 +0530

    Fix TypeScript import conflict in cricket-card component

--- src/components/cricket-card.tsx
+++ src/components/cricket-card.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from 'next/image'
-import { CricketCard } from '@/lib/data'
+import type { CricketCard } from '@/lib/data'
 import { cn } from '@/lib/utils'
 
 interface CricketCardProps {
